1950de2c7a3a894c3c7053a9bdae491cf8cda059
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-28 16:01:33 -0700

    Imported font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <h2>Van life~!</h2>
-  )
+    <>
+      <h2>Van life~!</h2>
+    </>
+  );
 }
 
-export default App
+export default App;
