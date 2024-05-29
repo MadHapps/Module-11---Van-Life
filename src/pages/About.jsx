a728d3d84088827ee01750952127e18f5022e486
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-29 12:54:42 -0700

    Created Home and About pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import "./styling/About.css"
+
+export default function About() {
+
+    return (
+        <main className="content-wrapper">
+            <h2>This is the About page~!</h2>
+        </main>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "./styling/Home.css"
+import Navbar from "../components/Navbar"
+
+export default function Home() {
+
+    return (
+        <main className="content-wrapper">
+            <h2>This is the Home page~!</h2>
+        </main>
+    )
+}
