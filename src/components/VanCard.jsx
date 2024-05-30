32caef230a8afe02360c22275dfdf4ffd697a477
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-30 13:14:34 -0700

    Created VanCard component

---
+++ src/components/VanCard.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import "./styling/VanCard.css"
+
+export default function VanCard({description, image, name, price, type}) {
+
+    const tag = type.charAt(0).toUpperCase() + type.slice(1)
+
+    return (
+        <div className="van-card-wrapper">
+            <img className="card-img" src={image} />
+            <div className="card-text-wrapper">
+                <h3 className="card-title">{name}</h3>
+                <p className="card-price">${price}</p>
+            </div>
+            <p className={`card-tag ${type ? type : 'default'}`}>{tag}</p>
+        </div>
+    )
+}
