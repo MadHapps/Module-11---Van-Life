aa0de19e3e2d9ab1c95354627f977766aed22a52
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-29 21:14:16 -0700

    Imported server to main.jsx, console.log'd fetch data for testing

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import "./styling/Vans.css";
 
 export default function Vans() {
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <>
       <h2>This is the Vans page~!</h2>
