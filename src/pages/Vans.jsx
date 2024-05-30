e73cca0b7ee59851abeb817fd9c7fc8f690dff68
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-30 13:15:39 -0700

    Populate Vans page with VanCard component using MirageJS server data

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,18 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./styling/Vans.css";
+import VanCard from "../components/VanCard";
 
 export default function Vans() {
+  const [vans, setVans] = useState();
+
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setVans(data.vans);
       });
   }, []);
 
+  console.log(vans);
+
   return (
-    <>
-      <h2>This is the Vans page~!</h2>
-    </>
+    <div className="van-page-wrapper">
+      {vans && vans.map((van) => (
+        <VanCard
+          key={van.id}
+          image={van.imageUrl}
+          name={van.name}
+          price={van.price}
+          type={van.type}
+        />
+      ))}
+    </div>
   );
 }
