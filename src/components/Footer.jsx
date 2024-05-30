e55da9b50137bfeb51bb8506228bc8d40db73676
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-29 22:18:39 -0700

    Moved work in progress message to footer, trying to resolve a horizontal white space problem

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
-import "./styling/Footer.css"
+import "./styling/Footer.css";
 
-export default function Footer({className}) {
-
-    return (
-        <footer className={className}>
-            <p>Ⓒ 2024 #VANLIFE</p>
-        </footer>
-    )
+export default function Footer({ className }) {
+  return (
+    <footer className={className}>
+      <p>Ⓒ 2024 #VANLIFE</p>
+      <div className="wip-wrapper">
+        <h4>Work in progress... exercise incomplete.</h4>
+      </div>
+    </footer>
+  );
 }
