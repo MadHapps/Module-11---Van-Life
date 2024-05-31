6eb87ff22bfcca13a689aee91c73ee1f0ea2def4
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-30 21:21:03 -0700

    Made filter buttons functional

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -1,18 +1,17 @@
 /* eslint-disable react/prop-types */
-import "./styling/VanCard.css"
+import "./styling/VanCard.css";
 
-export default function VanCard({description, image, name, price, type}) {
+export default function VanCard({ image, name, price, type }) {
+  const tag = type.charAt(0).toUpperCase() + type.slice(1);
 
-    const tag = type.charAt(0).toUpperCase() + type.slice(1)
-
-    return (
-        <div className="van-card">
-            <img className="card-img" src={image} />
-            <div className="card-text-wrapper">
-                <h3 className="card-title">{name}</h3>
-                <p className="card-price">${price}</p>
-            </div>
-            <p className={`card-tag ${type ? type : 'default'}`}>{tag}</p>
-        </div>
-    )
+  return (
+    <div className="van-card">
+      <img className="card-img" src={image} />
+      <div className="card-text-wrapper">
+        <h3 className="card-title">{name}</h3>
+        <p className="card-price">${price}</p>
+      </div>
+      <p className={`card-tag ${type ? type : "default"}`}>{tag}</p>
+    </div>
+  );
 }
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -4,13 +4,18 @@ import VanCard from "../components/VanCard";
 
 export default function Vans() {
   const [vans, setVans] = useState(null);
+  const [filteredVans, setFilteredVans] = useState(null);
+  const [filter, setFilter] = useState(null);
 
   useEffect(() => {
     const fetchVans = async () => {
       try {
         const response = await fetch("/api/vans");
         const data = await response.json();
-        setTimeout(() => setVans(data.vans), 918); // Delay for 638ms
+        setTimeout(() => {
+          setVans(data.vans);
+          setFilteredVans(data.vans);
+        }, 918);
       } catch (error) {
         console.error("Error fetching vans:", error);
       }
@@ -19,30 +24,71 @@ export default function Vans() {
     fetchVans();
   }, []);
 
+  useEffect(() => {
+    if (filter) {
+      setFilteredVans(vans.filter((van) => van.type === filter));
+    } else {
+      setFilteredVans(vans);
+    }
+  }, [filter, vans]);
+
+  function handleClick(type) {
+    setFilter(type);
+  }
+
+  function clearFilter() {
+    setFilter(null);
+  }
+
   return (
     <div className="van-page-wrapper">
       <div className="van-filter-options">
         <h2>Explore our van options</h2>
         <ul>
-          <li className="link-button default filter">Simple</li>
-          <li className="link-button default filter">Luxury</li>
-          <li className="link-button default filter">Rugged</li>
-          <a className="link-button">Clear filter</a>
+          <li
+            className={`link-button default filter ${
+              filter === "simple" ? "simple" : ""
+            }`}
+            onClick={() => handleClick("simple")}
+          >
+            Simple
+          </li>
+          <li
+            className={`link-button default filter ${
+              filter === "luxury" ? "luxury" : ""
+            }`}
+            onClick={() => handleClick("luxury")}
+          >
+            Luxury
+          </li>
+          <li
+            className={`link-button default filter ${
+              filter === "rugged" ? "rugged" : ""
+            }`}
+            onClick={() => handleClick("rugged")}
+          >
+            Rugged
+          </li>
+          <li className="link-button" onClick={clearFilter}>
+            Clear filter
+          </li>
         </ul>
       </div>
       <div className="van-card-wrapper">
-        {vans ? (
-          vans.map((van) => (
-            <VanCard
-              key={van.id}
-              image={van.imageUrl}
-              name={van.name}
-              price={van.price}
-              type={van.type}
-            />
-          ))
+        {filteredVans ? (
+          filteredVans.map((van) => {
+            return (
+              <VanCard
+                key={van.id}
+                image={van.imageUrl}
+                name={van.name}
+                price={van.price}
+                type={van.type}
+              />
+            );
+          })
         ) : (
-          <h3 className="van-cards-loading">Retrieving Vans</h3>
+          <h3 className="van-cards-loading">Retrieving Vans...</h3>
         )}
       </div>
     </div>
