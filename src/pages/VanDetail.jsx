01e0593c823ff2c1559285374a27c83672883df6
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-06-03 13:58:57 -0700

    Created VanDetail page

---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,10 @@
+import "./styling/VanDetail.css"
+
+export default function VanDetail() {
+
+    return (
+        <div className="van-detail-wrapper">
+            <h2>This is the Van Detail page~!</h2>
+        </div>        
+    )
+}
