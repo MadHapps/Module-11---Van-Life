3f0a77e9e20fe1fc07888a764689f127e28fea30
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-06-11 15:13:17 -0700

    Refactored Navbar to use NavLink instead of Link in order to take of advantage of the isActive prop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import "./styling/Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar({ className }) {
   return (
@@ -9,8 +9,24 @@ export default function Navbar({ className }) {
         #VANLIFE
       </Link>
       <div className="link-wrapper">
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink
+          to="/host"
+          className={({ isActive }) => (isActive ? "isActive" : null)}
+        >
+          Host
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "isActive" : null)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/vans"
+          className={({ isActive }) => (isActive ? "isActive" : null)}
+        >
+          Vans
+        </NavLink>
       </div>
     </nav>
   );
