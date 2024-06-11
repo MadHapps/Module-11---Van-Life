93a2c3b6437b36ef5190ee39faf3423bb9d3c299
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-06-11 15:14:40 -0700

    Created basis for Basis for the Host page and its subsequent subpages

---
+++ src/components/HostLayout.jsx
@@ -0,0 +1,40 @@
+import "./styling/Navbar.css";
+import "./styling/HostLayout.css";
+import { Outlet, NavLink } from "react-router-dom";
+
+export default function HostLayout() {
+  return (
+    <>
+      <nav className="host-nav-wrapper">
+        <div className="link-wrapper">
+          <NavLink
+            to="/host"
+            end
+            className={({ isActive }) => (isActive ? "isActive" : null)}
+          >
+            Dashboard
+          </NavLink>
+          <NavLink
+            to="income"
+            className={({ isActive }) => (isActive ? "isActive" : null)}
+          >
+            Income
+          </NavLink>
+          <NavLink
+            to="vans"
+            className={({ isActive }) => (isActive ? "isActive" : null)}
+          >
+            Vans
+          </NavLink>
+          <NavLink
+            to="reviews"
+            className={({ isActive }) => (isActive ? "isActive" : null)}
+          >
+            Reviews
+          </NavLink>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
